4788b17ceb349b3f252f5a8cc220854be22a6881
Beojan Stanislaus <beojan.stanislaus@physics.ox.ac.uk>
2018-09-21 13:47:21 +0100

    Update batchTemplates to LCG 94 matching setup.sh

--- analysis/boosted/tools/batchTemplate.sh
+++ analysis/boosted/tools/batchTemplate.sh
@@ -7,7 +7,7 @@
 # Setup stuff
 echo "Setting up"
 cd $CODEDIR
-source /cvmfs/sft.cern.ch/lcg/views/dev4/latest/x86_64-slc6-gcc7-opt/setup.sh
+source /cvmfs/sft.cern.ch/lcg/views/LCG_94/x86_64-slc6-gcc8-opt/setup.sh
 
 # Run
 echo "Running: "
