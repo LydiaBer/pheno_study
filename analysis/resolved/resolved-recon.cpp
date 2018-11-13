45b469d769350cee77cfe7b95b912b7d2262b2e9
Beojan Stanislaus <beojan@gmail.com>
2018-11-13 07:43:08 +0100

    **Why would you put control after sideband?**

--- analysis/resolved/resolved-recon.cpp
+++ analysis/resolved/resolved-recon.cpp
@@ -250,8 +250,8 @@ bool signal(const reconstructed_event& evt) {
     return (higgs1_flag && higgs2_flag);
 }
 
-// Select m<SUB>hh</SUB> sideband region
-bool sideband(const reconstructed_event& evt) {
+// Select m<SUB>hh</SUB> control region
+bool control(const reconstructed_event& evt) {
     double m_h1 = evt.higgs1.p4.M();
     double m_h2 = evt.higgs2.p4.M();
     // Cut in a mass window of 30 GeV around 125 GeV for both Higgs
@@ -260,8 +260,8 @@ bool sideband(const reconstructed_event& evt) {
     return (higgs1_flag && higgs2_flag);
 }
 
-// Select m<SUB>hh</SUB> control region
-bool control(const reconstructed_event& evt) {
+// Select m<SUB>hh</SUB> sideband region
+bool sideband(const reconstructed_event& evt) {
     double m_h1 = evt.higgs1.p4.M();
     double m_h2 = evt.higgs2.p4.M();
     // Cut in a mass window of 50 GeV around 125 GeV for both Higgs
@@ -269,6 +269,7 @@ bool control(const reconstructed_event& evt) {
     bool higgs2_flag = (std::abs(m_h2 - 125.) < 50.) ? true : false;
     return (higgs1_flag && higgs2_flag);
 }
+
 
 //***************
 // Main Analysis Code
