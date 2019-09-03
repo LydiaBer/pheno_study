941ddc58e93b02e30fcf61f68e68d4f1533341b1
Beojan Stanislaus <beojan.stanislaus@physics.ox.ac.uk>
2019-09-03 13:51:49 +0100

    Update file lists

--- analysis/resolved/runMe.py
+++ analysis/resolved/runMe.py
@@ -9,8 +9,10 @@ import os
 
 ### User inputs
 
-FILE_LIST = "../filelists/jesse_linked_delphes.txt" #2018sep13_all_merged_delphes.txt"
-OUT_DIR = os.getcwd().split("/resolved")[0]+"/outputs/jesse_linked_delphes"#2018sep13"
+TAG = "150719"
+FILE_LISTS = ["../filelists/signal_{0}.txt".format(TAG), "../filelists/background_{0}.txt".format(TAG)]
+OUT_DIR = os.getcwd().split("/resolved")[0]+"/outputs/"+TAG
+
 USE_BATCH = True
 
 ### End of user inputs
@@ -21,51 +23,36 @@ if not os.path.exists(OUT_DIR):
   os.makedirs(OUT_DIR+"/logs")
 
 # open filelist and read line by line
-with open(FILE_LIST, 'r') as filehandle:  
-  filecontents = filehandle.readlines()
+for FILE_LIST in FILE_LISTS:
+  with open(FILE_LIST, 'r') as filehandle:  
+    filecontents = filehandle.readlines()
 
-  for line in filecontents:
-      # remove linebreak which is the last character of the string
-      file_dir = line[:-1]
+    for file_path in filecontents:
       # skip if commented out with hash
-      if '#' in line: continue
+      if '#' in file_path: continue
       # skip if empty line
-      if len(line.strip()) == 0: continue
+      if len(file_path.strip()) == 0: continue
+      # remove spaces at the end of the line
+      file_path = file_path.strip()
 
       output_dir = OUT_DIR
-      # Get list of files in directory specified in file list
-      files = os.listdir(file_dir)
-   
-      # Loop over files in directory specified in FILE_LIST
-      for file in files: 
 
-        # make file path from file dir and file
-        file_path = file_dir+"/"+file
+      # append resolved to front of filename
+      output_filename = "resolved_"+file_path.split(TAG+'/')[1]
+      if "varied_coupling" in file_path:
+        TopYuk = file_path.split("TopYuk_")[1].replace("/","_")
+        output_filename = "resolved_noGenFilt_signal_hh_"+"TopYuk_"+TopYuk
+      command =  "./build/resolved-recon {0} {1} {2}".format(file_path, output_dir, output_filename)
+      print file_path
+      print output_filename
+      if USE_BATCH:
+        batch_script = os.getcwd()+"/tools/batchTemplate.sh"
+        logfile = OUT_DIR+"/logs/"+output_filename.split(".root")[0]
+        batch_command  = "qsub -N {0} -o {1} -e {2} -v CODEDIR='{3}',CMD='{4}' {5}".format(output_filename.split(".root")[0],logfile+".out",logfile+".err",os.getcwd(), command, batch_script)
+        print
+        print(batch_command)
+        os.system(batch_command)
+      else: 
+        print(command)
+        os.system(command)
 
-        # make short output filename of form resolved_genfiltercut_sample.root
-        if "signal/nominal" in file_path:
-          output_filename = "resolved_loop_hh_"+file   
-
-        elif "signal/varied_coupling" in file_path:
-          TopYuk = file_path.split("TopYuk_")[1].split("/SlfCoup")[0]
-          output_filename = "resolved_hh_"+"TopYuk_"+TopYuk+"_"+file  
-
-        elif "bkg" in file_path:
-          if "unfiltered" in file_path:
-            filter = "noGenFilt"
-          if "pt200" in file_path:
-            filter = "xpt200"
-          output_filename = "resolved_{0}_{1}_{2}".format(filter, file_path.split("/")[-2],file)
-
-        command =  "./build/resolved-recon {0} {1} {2}".format(file_path, output_dir, output_filename)
-        if USE_BATCH:
-          batch_script = os.getcwd()+"/tools/batchTemplate.sh"
-          logfile = OUT_DIR+"/logs/"+output_filename.split(".root")[0]
-          batch_command  = "qsub -N {0} -o {1} -e {2} -v CODEDIR='{3}',CMD='{4}' {5}".format(output_filename.split(".root")[0],logfile+".out",logfile+".err",os.getcwd(), command, batch_script)
-          print
-          print(batch_command)
-          os.system(batch_command)
-        else: 
-          print(command)
-          os.system(command)
-           
